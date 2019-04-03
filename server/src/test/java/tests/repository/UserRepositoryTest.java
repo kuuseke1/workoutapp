c968f7a951727eafe6c90b42db4a2f19df80ee8f
Emil <emil.fenenko@ttu.ee>
2019-04-03 15:30:03 +0300

    added couple repo tests

--- server/src/test/java/tests/repository/UserRepositoryTest.java
+++ server/src/test/java/tests/repository/UserRepositoryTest.java
@@ -7,10 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestComponent;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
--- server/src/test/java/tests/repository/WorkoutRepositoryTest.java
+++ server/src/test/java/tests/repository/WorkoutRepositoryTest.java
@@ -6,9 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
