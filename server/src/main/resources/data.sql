530e543a2404a04490edd07e4890d9ac1711e6f0
Emil <emil.fenenko@ttu.ee>
2019-04-03 05:00:19 +0300

    Added user repo, cleaned project structure, added dummy users

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+//dummy accounts
+INSERT INTO users
+values (10000, 180, 'Normal Dummy', 70); //for some reason this isnt getting added......
+
+INSERT INTO users
+values (1001, 150, 'Small Dummy', 50);
+
+INSERT INTO users
+values (1002, 200, 'Big Dummy', 100);
+
+
+//dummy workouts
+
+
